9ac1244d702abc122fe9a75f8a88cc3afd04cea7
gradyalgire <gdalgire04@gmail.com>
2026-02-05 11:47:52 -0500

    Fixed error
    Resolved error where playlist page wasnt displaying songs

--- getPlaylist.js
+++ getPlaylist.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 const supabase = createClient(
     'https://mqjeuhvltukwidrwecrt.supabase.co',
