7ec879a71ba746d54ec3cb910108a754418796de
gradyalgire <gdalgire04@gmail.com>
2026-05-02 18:44:40 -0400

    form testing

--- getRecommendation.js
+++ getRecommendation.js
@@ -45,92 +45,84 @@ function loadForm(type, clickedButton) {
     switch (type) {
         case "song":
             container.innerHTML = `
-                <form class="recommendation-form netlify">
-                    <fieldset>
-                        <legend>Song Details</legend>
-                        <div class="recommendation-form-group">
-                            <label for="title"><strong>Title</strong></label>
-                            <input type="text" id="title" name="title" required placeholder="e.g. The Mountain Is You">
-                        </div>
-                        <div class="recommendation-form-group">
-                            <label for="artist"><strong>Artist</strong></label>
-                            <input type="text" id="artist" name="artist" required placeholder="e.g. Chance Peña">
-                        </div>
-                        ${formTriggers}
-                        <div class="recommendation-form-group checkbox">
-                            <input type="checkbox" id="explicit" name="explicit">
-                            <label for="explicit">This song contains explicit content</label>
-                        </div>
-                        ${formReason}
-                        ${formSubmission}
-                    </fieldset>
-                </form>
+                <fieldset>
+                    <legend>Song Details</legend>
+                    <div class="recommendation-form-group">
+                        <label for="title"><strong>Title</strong></label>
+                        <input type="text" id="title" name="title" required placeholder="e.g. The Mountain Is You">
+                    </div>
+                    <div class="recommendation-form-group">
+                        <label for="artist"><strong>Artist</strong></label>
+                        <input type="text" id="artist" name="artist" required placeholder="e.g. Chance Peña">
+                    </div>
+                    ${formTriggers}
+                    <div class="recommendation-form-group checkbox">
+                        <input type="checkbox" id="explicit" name="explicit">
+                        <label for="explicit">This song contains explicit content</label>
+                    </div>
+                    ${formReason}
+                    ${formSubmission}
+                </fieldset>
             `;
             break;
         case "media":
             container.innerHTML = `
-                <form class="recommendation-form netlify">
-                    <fieldset>
-                        <legend>Media Details</legend>
-                        <div class="recommendation-form-group">
-                            <label for="type"><strong>Media type</strong></label>
-                            <select id="type" name="type">
-                                <option value="">-- Select Media Type --</option>
-                                <option value="podcast">Podcast</option>
-                                <option value="book">Book</option>
-                                <option value="video">Video</option>
-                                <option value="movie">Movie</option>
-                                <option value="quote">Quote</option>
-                            </select>
-                        </div>
-                        <div class="recommendation-form-group">
-                            <label for="title"><strong>Title</strong></label>
-                            <input type="text" id="title" name="title" required placeholder="e.g. Mindset Mentor">
-                        </div>
-                        <div class="recommendation-form-group">
-                            <label for="artist-author-creator"><strong>Artist / Author / Creator</strong></label>
-                            <input type="text" id="artist-author-creator" name="artist-author-creator" required placeholder="e.g. Rob Dial">
-                        </div>
-                        <div class="recommendation-form-group">
-                            <label for="link"><strong>Link</strong> (Spotify, Goodreads, Youtube, Letterboxd, etc...)</label>
-                            <input type="text" id="link" name="link" placeholder="https://...">
-                        </div>
-                        ${formTriggers}
-                        ${formReason}
-                        ${formSubmission}
-                    </fieldset>
-                </form>
+                <fieldset>
+                    <legend>Media Details</legend>
+                    <div class="recommendation-form-group">
+                        <label for="type"><strong>Media type</strong></label>
+                        <select id="type" name="type">
+                            <option value="">-- Select Media Type --</option>
+                            <option value="podcast">Podcast</option>
+                            <option value="book">Book</option>
+                            <option value="video">Video</option>
+                            <option value="movie">Movie</option>
+                            <option value="quote">Quote</option>
+                        </select>
+                    </div>
+                    <div class="recommendation-form-group">
+                        <label for="title"><strong>Title</strong></label>
+                        <input type="text" id="title" name="title" required placeholder="e.g. Mindset Mentor">
+                    </div>
+                    <div class="recommendation-form-group">
+                        <label for="artist-author-creator"><strong>Artist / Author / Creator</strong></label>
+                        <input type="text" id="artist-author-creator" name="artist-author-creator" required placeholder="e.g. Rob Dial">
+                    </div>
+                    <div class="recommendation-form-group">
+                        <label for="link"><strong>Link</strong> (Spotify, Goodreads, Youtube, Letterboxd, etc...)</label>
+                        <input type="text" id="link" name="link" placeholder="https://...">
+                    </div>
+                    ${formTriggers}
+                    ${formReason}
+                    ${formSubmission}
+                </fieldset>
             `;
             break;
         case "trigger":
             container.innerHTML = `
-                <form class="recommendation-form netlify">
-                    <fieldset>
-                        <legend>Trigger Details</legend>
-                        <div class="recommendation-form-group">
-                            <label for="target-title"><strong>Which song or media item is this trigger for?</strong></label>
-                            <input type="text" id="target-title" name="target_title" placeholder="e.g. The Mountain Is You - Chance Peña">
-                        </div>
-                        ${formReason}
-                        ${formSubmission}
-                    </fieldset>
-                </form>
+                <fieldset>
+                    <legend>Trigger Details</legend>
+                    <div class="recommendation-form-group">
+                        <label for="target-title"><strong>Which song or media item is this trigger for?</strong></label>
+                        <input type="text" id="target-title" name="target_title" placeholder="e.g. The Mountain Is You - Chance Peña">
+                    </div>
+                    ${formReason}
+                    ${formSubmission}
+                </fieldset>
             `;
             break;
         case "other":
             container.innerHTML = `
-                <form class="recommendation-form netlify">
-                    <fieldset>
-                        <legend>Your Recommendation</legend>
-                        <div class="recommendation-form-group">
-                            <label for="suggestion"><strong>What are you suggesting?</strong></label>
-                            <p class="recommendation-form-note">Suggest anything that does not fit into the other categories.</p>
-                            <textarea id="suggestion" name="suggestion" rows="5" required placeholder="Describe your recommendation"></textarea>
-                        </div>
-                        ${formReason}
-                        ${formSubmission}
-                    </fieldset>
-                </form>
+                <fieldset>
+                    <legend>Your Recommendation</legend>
+                    <div class="recommendation-form-group">
+                        <label for="suggestion"><strong>What are you suggesting?</strong></label>
+                        <p class="recommendation-form-note">Suggest anything that does not fit into the other categories.</p>
+                        <textarea id="suggestion" name="suggestion" rows="5" required placeholder="Describe your recommendation"></textarea>
+                    </div>
+                    ${formReason}
+                    ${formSubmission}
+                </fieldset>
             `;
             break;
         default:
